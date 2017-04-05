80afd7f67272181c014417add227630b2114fab0
Ben Williams and Sara Schultz <ben+sara@>
2017-04-05 15:33:52 -0700

    add working project w/o constructor, pre-Lena

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,9 +1,11 @@
-// function Player(roundRolls, totalScore) {
+// function Player(roundRolls, roundScore) {
 //   this.roundRolls = [];
-//   this.totalScore = totalScore;
+//   this.roundScore = roundScore;
 // }
 
 var roundSubTotal = 0;
+var totalScore = 0;
+
 
 var randomRoll = function() {
   var retRoll = (Math.floor(Math.random() * 6) + 1);
@@ -11,10 +13,21 @@ var randomRoll = function() {
   return retRoll;
 }
 
-function roundRolls(randomRoll) {
-roundSubTotal += randomRoll;
-console.log(roundSubTotal);
-return roundSubTotal;
+function roundRolls(numba) {
+  roundSubTotal += numba;
+  console.log(roundSubTotal);
+    if (numba === 1) {
+      roundSubTotal = 0
+      alert("you busted!");
+    }
+  return roundSubTotal;
+}
+
+function ruhRoh(roundScore) {
+  totalScore += roundScore;
+  console.log(totalScore);
+
+  return totalScore;
 }
 
 //Front-End Logic:
@@ -22,8 +35,19 @@ $(document).ready(function(){
   $("#rollForm").submit(function(e){
     e.preventDefault();
     var currentScore = randomRoll();
-    var totalScore = roundRolls(currentScore);
+    var roundScore = roundRolls(currentScore);
+
     $("#dieRoll").append("<li>" + currentScore + "</li>");
-    $("#totalScore").text(totalScore);
+    $("#roundScore").text(roundScore);
+    $("#holdButton").click(function(){
+      // e.preventDefault();
+      // var currentScore = randomRoll();
+      // var roundScore = roundRolls(currentScore);
+      var piggyBank = roundScore;
+      console.log(piggyBank);
+  
+    });
+
   });
+
 });
