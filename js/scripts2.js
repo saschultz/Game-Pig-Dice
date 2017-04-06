43a78f0e5ea8f83620878a405950de15d3dcc0c2
Ben Williams and Sara Schultz <ben@>
2017-04-06 12:02:04 -0700

    add working iteration of constructors and round total.

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,19 +1,23 @@
 function Player() {
-  this.name = name;
-  this.roundSubTotal = [];
+  this.name = "";
+  this.roundSubTotal = 0;
   this.total = [];
+  this.currentRoll;
 }
 
 Player.prototype.roll = function() {
-  var retRoll = (Math.floor(Math.random() * 6) + 1);
-  console.log(retRoll);
-  this.roundSubTotal.push(retRoll);
-  console.log(this.roundSubTotal);
-  return retRoll;
+  this.currentRoll = (Math.floor(Math.random() * 6) + 1);
+  // console.log(retRoll);
+  this.roundSubTotal+=this.currentRoll;
 }
 
-Player.prototype.roundRolls = function(numba) {
-  roundSubTotal += numba;
+Player.prototype.playerSubTotal = function () {
+  // var roundScore = roundScore.push(this.roundSubTotal);
+  //
+  // return roundScore;
+}
+
+Player.prototype.playerTotal = function() {
   console.log(roundSubTotal);
     if (numba === 1) {
       roundSubTotal = 0
@@ -24,18 +28,23 @@ Player.prototype.roundRolls = function(numba) {
 
 //Front-End Logic:
 $(document).ready(function(){
+  var userName = $()
   var newPlayer = new Player();
   $("#rollForm").submit(function(e){
+
     e.preventDefault();
+    // var userRoundSubTotal = newPlayer.roll()
     // this.roundSubTotal = newPlayer.push(newPlayer.roll());
     // console.log(roundSubTotal);
-
-    $("#dieRoll").append("<li>" + newPlayer.roll() + "</li>");
-
+    newPlayer.roll();
+    $("#dieRoll").append("<li>" + newPlayer.currentRoll + "</li>");
+    $("#roundScore").text(newPlayer.roundSubTotal);
   });
   // $("#holdButton").click(function(){
   //   newPlayer.roundScore = newPlayer.subTotal;
   //   var piggyBank = roundScore;
   //   console.log(piggyBank);
+
+  // var addedSub = newPlayer.playerSubTotal();
   // });
 });
