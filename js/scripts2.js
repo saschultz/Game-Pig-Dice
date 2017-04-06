a5c8c2371d3c7137d072426e2e38258ba63123f0
Ben Williams and Sara Schultz <sara@>
2017-04-06 14:13:55 -0700

    add functionality for player 2 and alert winner.

--- js/scripts2.js
+++ js/scripts2.js
@@ -7,43 +7,54 @@ function Player() {
 
 Player.prototype.roll = function() {
   this.currentRoll = (Math.floor(Math.random() * 6) + 1);
-  // console.log(retRoll);
+  if (this.currentRoll === 1) {
+    this.roundSubTotal = 0;
+    $("#dieRoll").empty();
+    alert("Whoopsy! You rolled a 1");
+  } else {
   this.roundSubTotal+=this.currentRoll;
+  }
+  console.log(this.currentRoll);
 }
 
 Player.prototype.playerTotal = function () {
-  // var roundScore = roundScore.push(this.roundSubTotal);
-  //
-  // return roundScore;
   this.total+=this.roundSubTotal;
+  if (this.total >= 100) {
+    alert("Winner Winner!");
+  }
   console.log(this.roundSubTotal);
 }
 
-// Player.prototype.player = function() {
-//   console.log(roundSubTotal);
-//     if (numba === 1) {
-//       roundSubTotal = 0
-//       alert("you busted!");
-//     }
-//   return roundSubTotal;
-// }
-
 //Front-End Logic:
 $(document).ready(function(){
-  var newPlayer = new Player();
+  var newPlayer1 = new Player();
+  var newPlayer2 = new Player();
   $("#rollForm").submit(function(e){
     e.preventDefault();
-    newPlayer.roll();
-    $("#dieRoll").append("<li>" + newPlayer.currentRoll + "</li>");
-    $("#roundScore").text(newPlayer.roundSubTotal);
+    newPlayer1.roll();
+    // $("#dieRoll").append("<li>" + newPlayer1.currentRoll + "</li>");
+    $("#roundScore").text(newPlayer1.roundSubTotal);
 
   });
   $("#holdButton").click(function(){
     $("#roundScore").empty();
-    newPlayer.playerTotal();
-    $("#piggyBank").text(newPlayer.total);
-    newPlayer.roundSubTotal = 0;
+    $("#dieRoll").empty();
+    newPlayer1.playerTotal();
+    $("#piggyBank").text(newPlayer1.total);
+    newPlayer1.roundSubTotal = 0;
+  });
+  $("#rollForm2").submit(function(e){
+    e.preventDefault();
+    newPlayer2.roll();
+    // $("#dieRoll").append("<li>" + newPlayer2.currentRoll + "</li>");
+    $("#roundScore2").text(newPlayer2.roundSubTotal);
 
-    // var addedSub = newPlayer.playerSubTotal();
+  });
+  $("#holdButton2").click(function(){
+    $("#roundScore2").empty();
+    $("#dieRoll2").empty();
+    newPlayer2.playerTotal();
+    $("#piggyBank2").text(newPlayer2.total);
+    newPlayer2.roundSubTotal = 0;
   });
 });
